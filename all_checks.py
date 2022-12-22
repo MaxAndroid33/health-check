c8f3dc4f9c181e9e7933671d7af677f23da0bc86
MaxAndroid33 <53229935+MaxAndroid33@users.noreply.github.com>
2022-12-22 22:16:21 +0300

    Add disk full check to all_check.py

--- all_checks.py
+++ all_checks.py
@@ -1,10 +1,23 @@
 #!/user/bin/env python3
 import os
+import shutil
 import sys
 
 def check_reboot():
     """ Returns True if the computer has a pending erboot. """
-    
+    return os.path.exits("/run/reboot-required")
+
+def check_disk_usage(disk ,min_abs ,mi_percent):
+    """Returns True if there is enough free disk space , false otherwise """
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free /du.total
+    # Calculate how many free gigabytes
+    gigabytes_free =du.free /2**30
+    if percent_free <mi_percent or gigabytes_free < min_abs :
+        return False
+    return True
+
 def main():
     if check_reboot():
         print("pending Reboot.")
